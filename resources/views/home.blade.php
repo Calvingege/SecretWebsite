6fb4778806b8c2fd38ae73c4df846f01a27ca147
Calvin Gerald <calvingege>
2022-10-25 19:31:21 +0700

    commitpertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link 
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+        crossorigin="anonymous"
+    />
+</head>
+<body>
+
+<!-- Navbar -->
+<nav id="navbar-example2" class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">Secret Website</a>
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <a class="nav-link" href="#purchasesapi">pembelian sapi</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#mdo">Category Sapi</a>
+  </ul>
+</nav>
+<!-- End Of Navbar -->
+
+
+<div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+  <h4 id="fat">@purchasesapi</h4>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem.
+  <!-- To do, bikin halaman purchaseSapi -->
+  <a href="/purchase">PurchaseHere</a>
+  </p>
+
+  <h4 id="mdo">@mdo</h4>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem tenetur, dignissimos illo ipsam neque similique quibusdam magni suscipit aspernatur, enim libero, culpa voluptatum veniam. Culpa nihil non quo cum laborum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem.</p>
+  <h4 id="one">Paragraph</h4>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt totam voluptate voluptates rerum in, optio veritatis illum exercitationem debitis praesentium sequi necessitatibus eveniet reprehenderit repellendus alias voluptatem corporis pariatur non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem.</p>
+</div>
+
+
+
+</form>
+
+</body>
+</html>
---
+++ resources/views/purchaseSapi.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>purchaseSapi</title>
+</head>
+<body>
+    <div class="text-center">
+        <h1>Beli sapi disini woiii</h1>
+        <img src="../Asset/sapicokelat.jpg" alt="sapicokelat">
+    </div>
+</body>
+</html>
