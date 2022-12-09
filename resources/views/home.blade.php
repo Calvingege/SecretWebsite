4106470909c4c6fee831d2c521bb79ba6ad4524c
RoARene317 <herendra.kurniawan@gmail.com>
2022-12-10 00:43:49 +0700

    Editing layout
    Editing to make it a little bit attractive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,48 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sapi kualitas papan atas!</title>
     <link 
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
         crossorigin="anonymous"
     />
+    <link rel="style" href="style_home.css">
+    
 </head>
 <body>
 
 <!-- Navbar -->
 <nav id="navbar-example2" class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">Sapi Pink</a>
+  <a class="navbar-brand" href="#">Secret Website</a>
   <ul class="nav nav-pills">
     <li class="nav-item">
-      <a class="nav-link" href="/purchase/sapi">pembelian sapi</a>
+      <a class="nav-link" href="#purchasesapi">pembelian sapi</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="/login">Account</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="">contact admin</a>
+      <a class="nav-link" href="#mdo">Category Sapi</a>
   </ul>
 </nav>
 <!-- End Of Navbar -->
 
 
-<div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+<div data-spy="scroll" data-target="#navbar-example2" data-offset="0" class="d-flex flex-column align-items-end justify-content-center border border-dark">
   <h4 id="fat">@purchasesapi</h4>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem.
+  <p>Selamat Datang di Situs kami, tempat penjualan sapi.
   <!-- To do, bikin halaman purchaseSapi -->
-  <a href="/purchase/sapi">PurchaseHere</a>
   </p>
-
-  <h4 id="mdo">@mdo</h4>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem tenetur, dignissimos illo ipsam neque similique quibusdam magni suscipit aspernatur, enim libero, culpa voluptatum veniam. Culpa nihil non quo cum laborum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem.</p>
-  <h4 id="one">Paragraph</h4>
-  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt totam voluptate voluptates rerum in, optio veritatis illum exercitationem debitis praesentium sequi necessitatibus eveniet reprehenderit repellendus alias voluptatem corporis pariatur non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit sint iusto, quibusdam eligendi cupiditate dolore provident nisi quia delectus earum a libero. Labore excepturi, reiciendis id odio at voluptatem.</p>
+  <h4 id="mdo"></h4>
+  <button type="button" class="btn btn-primary" href="/purchase">Purchase Here</button>
 </div>
-
-
-
+<div data-spy="scroll" data-target="#navbar-example2" data-offset="0" class="">
+  <p>Sapi berkualitas semuanya ada disini.</p>
+  <h4 id="one"></h4>
+</div>
+<div data-spy="scroll" data-target="#navbar-example2" data-offset="0" class="d-flex flex-column justify-content-center border border-dark">
+Buruan Beli sekarang juga!
+</div>
 </form>
 
 </body>
